ccfa6101b297a9510c3aac95021eb085b8c0aa8f
Shreyas Ashtamkar <shreyas.ashtamkar18@gmail.com>
2026-09-02 23:24:27 +0530

    Update model initialization and enhance conversation structure for translation functionality

--- src/agent_learn_1/app.py
+++ src/agent_learn_1/app.py
@@ -1,10 +1,28 @@
 # from langchain.chat_models import 
 from dotenv import load_dotenv
 from langchain_community.llms.deepinfra import DeepInfra
+
 load_dotenv()
 
-model = DeepInfra(name="ibm-granite/granite-4.2-3b")
+model = DeepInfra(name="openai/gpt-oss-20b", model_kwargs={
+    "system_prompt": "Be a helpful assistant, that translates English to French.",
+    "max_new_tokens" : 64,
+    "temperature": 0.3,
+    "top_p": 0.9,
+    "min_p": 0.3,
+    "top_k": 7,
+    "repetition_penalty": 2,
+    "presence_penalty": 0,
+    "frequency_penalty": 0,
+    "service_tier": "default",
+})
 
 def main() -> None:
-    result = model.invoke("Hello, world!")
+    conv = [
+        # { "role": "system",    "content": "You are a helpful assistant that translates English to French." },
+        { "role": "user",      "content": "Translate: I love programming." },
+        { "role": "assistant", "content": "J'adore la programmation." },
+        { "role": "user",      "content": "Translate: I love agents" },
+    ]
+    result = model.invoke(conv)
     print(result)
